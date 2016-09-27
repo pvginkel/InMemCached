af730375f4ca31fa84f35f9f13ab1e54f50f943d
Pieter van Ginkel <pvginkel@gmail.com>
2016-09-27 18:26:05 +0200

    Created version 1.0.1.0.

--- InMemCached/Properties/AssemblyInfo.cs
+++ InMemCached/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("95ab521f-aeae-4cc3-8c96-e5aae320cdbe")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
